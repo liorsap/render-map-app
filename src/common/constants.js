90c35a00c7ceb77ad8c8387d031e387f7edb9491
liorsap <liorsap@users.noreply.github.com>
2025-01-19 18:52:21 +0200

    update MAP_BOUNDS

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,9 +1,9 @@
 // Bounds for map.svg representation of Rome
 export const MAP_BOUNDS = {
-    minLng: 12.1790,
-    maxLng: 12.6398,
-    minLat: 41.7431,
-    maxLat: 41.9865
+    minLng: 12.1100,
+    maxLng: 12.7540,
+    minLat: 41.744,
+    maxLat: 41.9785
 };
 
 export const ROME_CENTER = [12.511330, 41.891930];
